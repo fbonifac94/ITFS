af43935ac4328da621449ce9875a2f09bfc110d4
fbonifac94 <facu.bonifacich@hotmail.com>
2019-09-12 20:15:26 -0300

    Ejericio 12-09

---
+++ ejuno.py
@@ -0,0 +1,20 @@
+import sys;
+import argparse;
+import os;
+
+parser = argparse.ArgumentParser();
+parser.add_argument("-f", "--filename");
+args = parser.parse_args();
+
+try:
+    fileO = open(args.filename, "r+");
+    parrafos = fileO.readlines();
+    newFile = open("newfile.txt", "a+")
+    
+    for z in parrafos:
+        z = z.replace(",", "|");
+        newFile.write(z);
+        break;
+except Exception as e:
+    print(e);
+    
